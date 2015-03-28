268df438dab10c454b457900f6bdb0e2ba513714
airled <airled@mail.ru>
2015-03-28 09:45:46 +0300

    from home

--- urls_category.rb
+++ urls_category.rb
@@ -1,9 +1,9 @@
-#collecting category URLs from the page and collecting them in a MySQL table
+#fetching category URLs from the page and collecting them in a MySQL table
 require 'sequel'
 require 'mysql2'
 require 'nokogiri'
 require 'open-uri'
-#getting the page html code
+#fetching the HTML code
 doc = Nokogiri::HTML(open('http://www.catalog.onliner.by'))
 #collecting URLs in the array
 xrequest="//table[@class='fphotblock add_line_main_menu']//div/a[1]/@href"
