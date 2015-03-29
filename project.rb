4643016e949c6273216dcdb88fae75bea162abfc
airled <airled@mail.ru>
2015-03-29 20:44:29 +0300

    correct

--- project.rb
+++ project.rb
@@ -1,24 +1,26 @@
-#fetching category URLs from the page and collecting them in a MySQL table
 require 'sequel'
 require 'mysql2'
 require 'nokogiri'
 require 'open-uri'
 
 #fetching HTML code
-page = Nokogiri::HTML(open('http://www.catalog.onliner.by'))
+html = Nokogiri::HTML(open('http://www.catalog.onliner.by'))
 
+#GROUPS
 #collecting group names in an array
 xgroup_names_ru = "//h1[@class='cm__h1']"
-group_names_ru = page.xpath(xgroup_names_ru).map{ |name| name.text.delete("0-9") }
+group_names_ru = html.xpath(xgroup_names_ru).map{ |name| name.text.delete("0-9") }
 
+#CATEGORIES
 #collecting category URLs in an array
 xcategory_urls = "//table[@class='fphotblock add_line_main_menu']//div/a[1]/@href"
-category_urls = page.xpath(xcategory_urls).map{ |link| link.value }.uniq
+category_urls = html.xpath(xcategory_urls).map{ |link| link.value }.uniq
 
 #connecting to a MySQL database
 file = File.open("#{Dir.pwd}/config")
 values = []
 file.each_line { |line| values.push line.chomp }
+file.close
 parameters = [:adapter,:user,:password,:host,:database]
 connect_parameters = Hash[parameters.zip(values)]
 DB = Sequel.connect(connect_parameters)
