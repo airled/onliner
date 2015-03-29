3b294e1be6c11cfbc0c492dcbc1663fa38e16394
airled <airled@mail.ru>
2015-03-29 09:05:58 +0300

    123

--- urls_category.rb
+++ urls_category.rb
@@ -3,15 +3,20 @@ require 'sequel'
 require 'mysql2'
 require 'nokogiri'
 require 'open-uri'
+
 #fetching HTML code
 doc = Nokogiri::HTML(open('http://www.catalog.onliner.by'))
+
 #collecting URLs in an array
 xrequest="//table[@class='fphotblock add_line_main_menu']//div/a[1]/@href"
 urls = doc.xpath(xrequest).map{ |link| link.value }.uniq
+
 #connecting to a MySQL database
 DB=Sequel.connect(:adapter=>'mysql2', :user=>'root', :host=>'localhost', :database=>'test')
+
 #creating a model
 class Url < Sequel::Model
 end
+
 #inserting the URLs into the MySQL table
 urls.map { |url| Url.create(:url=>url) }
