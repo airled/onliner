73c03b28adcc64a94a1fe7bc598c0d871d0ac61a
airled <airled@mail.ru>
2015-03-30 16:54:53 +0300

    end

--- project.rb
+++ project.rb
@@ -18,9 +18,9 @@ groups = html.xpath(xgroups).map{ |node| node.text.delete("0-9") }
 xcategories = "//table[@class='fphotblock add_line_main_menu']//div[@class='i']"
 categories = html.xpath(xcategories).map do |node|
   url = node.xpath("./a[1]/@href").text
-  name = node.xpath("./a[last()]").text
+  name_ru = node.xpath("./a[last()]").text
   is_new = node.xpath("./a[2]/img[@class='img_new']").any?
-  {:name=>name, :url=>url, :is_new=>is_new}
+  {:name_ru=>name_ru, :url=>url, :is_new=>is_new}
 end
 
 #creating a model
