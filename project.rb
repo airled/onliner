b20f7f8b7000ab799fe07599701d0bfa4f1a1b55
airled <airled@mail.ru>
2015-03-30 10:22:29 +0300

    add isnew

--- project.rb
+++ project.rb
@@ -2,19 +2,27 @@ require 'sequel'
 require 'mysql2'
 require 'nokogiri'
 require 'open-uri'
+#require 'logger'
 
 #fetching HTML code
 html = Nokogiri::HTML(open('http://www.catalog.onliner.by'))
 
 #GROUPS
 #collecting group names in an array
-xgroup_names_ru = "//h1[@class='cm__h1']"
-group_names_ru = html.xpath(xgroup_names_ru).map{ |name| name.text.delete("0-9") }
+xgroups = "//h1[@class='cm__h1']"
+groups = html.xpath(xgroups).map{ |node| node.text.delete("0-9") }
 
 #CATEGORIES
 #collecting category URLs in an array
-xcategory_urls = "//table[@class='fphotblock add_line_main_menu']//div/a[1]/@href"
-category_urls = html.xpath(xcategory_urls).map{ |link| link.value }.uniq
+xcategories = "//table[@class='fphotblock add_line_main_menu']//div[@class='i']"
+categories = html.xpath(xcategories).map do |node|
+  url = node.xpath("./a[1]/@href").text
+  name = node.xpath("./a[last()]").text
+  isnew = if node.xpath("./a[2]/img/@class='img_new'")
+    true || false
+  end
+  {:name=>name, :url=>url, :is_new=>isnew}
+end
 
 #connecting to a MySQL database
 file = File.open("#{Dir.pwd}/config")
@@ -24,16 +32,20 @@ file.close
 parameters = [:adapter,:user,:password,:host,:database]
 connect_parameters = Hash[parameters.zip(values)]
 DB = Sequel.connect(connect_parameters)
+#DB.loggers << Logger.new($stdout)
 
 #creating a model
 class Group < Sequel::Model
+  many_to_many :categories
+  one_through_one :category
 end
 class Category < Sequel::Model
 end
 class Product < Sequel::Model
 end
 
-group_names_ru.map { |name| Group.create(:name_ru => name) }
+#inserting names of the groups
+groups.map { |name| Group.create(:name_ru => name) }
 
-#inserting the URLs into the MySQL table
-category_urls.map { |url| Category.create(:url => url) }
+#inserting category values
+categories.map { |hash| Category.create(hash) }
--- temp.rb
+++ temp.rb
@@ -2,16 +2,14 @@ require 'nokogiri'
 require 'open-uri'
 
 page = Nokogiri::HTML(open('http://www.catalog.onliner.by'))
+page_file=File.open('/home/air/Desktop/1.txt','w')
+page_file.puts page
 
-a=[]
 xcategory = "//table[@class='fphotblock add_line_main_menu']//div[@class='i']"
 category = page.xpath(xcategory).map do |node|
   url = node.xpath("./a[1]/@href").text
   name = node.xpath("./a[last()]").text
-  h={:name=>name, :url=>url}
-  a.push h
+  {:name=>name, :url=>url}
 end
-puts a
-  
-http://catalog.onliner.by/pic/ico-mcat682blue.gif
-http://catalog.onliner.by/pic/restyle_3/new.gif
+
+puts category
