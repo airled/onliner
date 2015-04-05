2cc94ba1cdc1de420f5c2b4415ee8812abb9e25a
airled <airled@mail.ru>
2015-04-05 15:11:52 +0300

    adjust

--- project.rb
+++ project.rb
@@ -51,7 +51,7 @@ groups.zip(categories_blocks).map do |group_node, categories_block|
         category.add_product(product)
       end
       html_product.xpath("//a").map do |is_next_node|
-        if is_next_node.text.include?("Следующие" && "позиций")
+        if is_next_node.text.delete(" " "\n" "0-9").include? "Следующиепозиций"
           url_product = url_product_const + is_next_node.xpath("./@href").to_s
           break
         else url_product = false
