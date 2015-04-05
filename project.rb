467cea4db9cae249f59f441bf8d4d1327d7b1350
airled <airled@mail.ru>
2015-04-05 13:40:01 +0300

    modified

--- project.rb
+++ project.rb
@@ -23,12 +23,40 @@ def create_category(category_node)
   Category.create(name: name, name_ru: name_ru, url: url, is_new: is_new)
 end
 
+#creating products in the Products mysql table
+def create_product(product_node)
+  url = product_node.xpath("./strong/a/@href")
+  name = product_node.xpath("./strong/a").text.delete("\n").delete(" ")
+  Product.create(url: url, name: name)
+end
+
+
 groups = html.xpath("//h1[@class='cm__h1']")
 categories_blocks = html.xpath("//ul[@class='b-catalogitems']")
+
 groups.zip(categories_blocks).map do |group_node, categories_block|
   group = create_group(group_node)
   categories_block.xpath("./li/div[@class='i']").map do |category_node|
     category = create_category(category_node)
     group.add_category(category)
+    
+    url_product = category_node.xpath("./a[1]/@href").text
+    url_product_const = 'http://catalog.onliner.by/'
+
+    while url_product do
+
+      html_product = Nokogiri::HTML(open(url_prod))
+      html_product.xpath("//tr/td[@class='pdescr']").map do |product_node|
+        product = create_product(product_node)
+        category.add_product(product)
+      end
+      html_product.xpath("//a").map do |is_next_node|
+        url_product = if is_next_node.text.delete("\n").delete(" ") == "Следующие15позиций"
+          url_product_const + is_next_node.xpath("./@href").to_s
+        end || false
+      end
+
+    end
+
   end
-end 
+end
--- temp1.rb
+++ temp1.rb
@@ -1,42 +1,64 @@
-require 'sequel'
-require 'mysql2'
+require './init_db'
+require './init_models'
 require 'nokogiri'
 require 'open-uri'
 require 'logger'
+#DB.loggers << Logger.new($stdout)
 
-database=Sequel.connect(:adapter=>'mysql2', :host=>'localhost', :user=>'root', :database=>'test')
-
-#database.loggers << Logger.new($stdout)
-
-database.run('drop table if exists prods')
-
-database.create_table :prods do
-  primary_key :id
-  String :url
-  String :name, :size=>20
+#fetching HTML code
+html = Nokogiri::HTML(open('http://www.catalog.onliner.by'))
+ 
+#creating groups in the Groups mysql table
+def create_group(group_node)
+  name = group_node.text.delete("0-9")
+  Group.create({ name_ru: name })
 end
 
-class Prod < Sequel::Model
+#creating categories in the Categories mysql table
+def create_category(category_node)
+  url = category_node.xpath("./a[1]/@href").text
+  name = url.sub("http://catalog.onliner.by/","").delete('/')
+  name_ru = category_node.xpath("./a[last()]").text
+  is_new = category_node.xpath("./a[2]/img[@class='img_new']").any?
+  Category.create(name: name, name_ru: name_ru, url: url, is_new: is_new)
 end
 
-url = 'http://catalog.onliner.by/antivirus/'
-url_const = 'http://catalog.onliner.by/'
+#creating products in the Products mysql table
+def create_product(product_node)
+  url = product_node.xpath("./strong/a/@href")
+  name = product_node.xpath("./strong/a").text.delete("\n").delete(" ")
+  Product.create(url: url, name: name)
+end
 
-while url do
-  html = Nokogiri::HTML(open(url))
-  
-  html.xpath("//tr/td[@class='pdescr']").map do |prod_node|
-    url = prod_node.xpath("./strong/a/@href")
-    name = prod_node.xpath("./strong/a").text.delete("\n").delete(" ")
-    aproduct=Prod.create(:url=>url, :name=>name)
+
+groups = html.xpath("//h1[@class='cm__h1']")
+categories_blocks = html.xpath("//ul[@class='b-catalogitems']")
+
+groups.zip(categories_blocks).map do |group_node, categories_block|
+  group = create_group(group_node)
+  categories_block.xpath("./li/div[@class='i']").map do |category_node|
+    category = create_category(category_node)
+    group.add_category(category)
+    
+    url_prod = category_node.xpath("./a[1]/@href").text
+    url_prod_const = 'http://catalog.onliner.by/'
+
+    while url_prod do
+
+      html_product = Nokogiri::HTML(open(url_prod))
+      html_product.xpath("//tr/td[@class='pdescr']").map do |product_node|
+        product = create_product(product_node)
+        category.add_product(product)
+      end
+      html_product.xpath("//a").map do |is_next_node|
+        if is_next_node.text.delete("\n").delete(" ") == "Следующие15позиций"
+          url_prod = url_prod_const + is_next_node.xpath("./@href").to_s
+          break
+        else url_prod = false
+        end
+      end
+
+    end     
+
   end
-
-  html.xpath("//a").map do |node|
-    if node.text.delete("\n").delete(" ") == "Следующие15позиций"
-      url = url_const + node.xpath("./@href").to_s
-      puts url
-    break
-    else url = false
-    end
-  end
-end
+end 
