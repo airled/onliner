bfec9e7d0df1e9789ac03a0dd461293664f33cdf
airled <airled@mail.ru>
2015-04-06 17:11:27 +0300

    with global

--- temp.rb
+++ temp.rb
@@ -2,57 +2,67 @@ require './init_db'
 require './init_models'
 require 'nokogiri'
 require 'open-uri'
-require 'logger'
-#DB.loggers << Logger.new($stdout)
+#require 'logger'; DB.loggers << Logger.new($stdout)
 
 #fetching HTML code
-html = Nokogiri::HTML(open('http://www.catalog.onliner.by'))
+$url_const = "http://catalog.onliner.by"
+html = Nokogiri::HTML(open($url_const))
  
-#creating groups in the Groups mysql table
+#creating groups in Groups table
 def create_group(group_node)
   name = group_node.text.delete("0-9")
   Group.create({ name_ru: name })
 end
 
-#creating categories in the Categories mysql table
+#creating categories in Categories table
 def create_category(category_node)
   url = category_node.xpath("./a[1]/@href").text
-  name = url.sub("http://catalog.onliner.by/","").delete('/')
+  name = url.sub($url_const,"").delete('/')
   name_ru = category_node.xpath("./a[last()]").text
   is_new = category_node.xpath("./a[2]/img[@class='img_new']").any?
   Category.create(name: name, name_ru: name_ru, url: url, is_new: is_new)
 end
 
+#creating products in Products table
+def create_product(product_node)
+  url = $url_const + product_node.xpath("./strong/a/@href").text
+  name = product_node.xpath("./strong/a").text.delete("\n" " ")
+  image_url = product_node.xpath("../td[@class='pimage']/a/img/@src").text
+  Product.create(url: url, name: name, image_url: image_url)
+end
+
+#getting group and common category nodes
 groups = html.xpath("//h1[@class='cm__h1']")
 categories_blocks = html.xpath("//ul[@class='b-catalogitems']")
 
+#matching category to its group
 groups.zip(categories_blocks).map do |group_node, categories_block|
   group = create_group(group_node)
   categories_block.xpath("./li/div[@class='i']").map do |category_node|
     category = create_category(category_node)
     group.add_category(category)
     
-    url_prod = category_node.xpath("./a[1]/@href").text
-    url_prod_const = 'http://catalog.onliner.by/'
+    url_product = category_node.xpath("./a[1]/@href").text
 
-    while url_prod do
-
-      html_product = Nokogiri::HTML(open(url_prod))
-      html_product.xpath("//tr/td[@class='pdescr']").map do |prod_node|
-        url = prod_node.xpath("./strong/a/@href")
-        name = prod_node.xpath("./strong/a").text.delete("\n").delete(" ")
-        aproduct=Product.create(:url=>url, :name=>name)
-        category.add_product(aproduct)
+    while url_product do
+    
+      #searching for the product parameters
+      html_product = Nokogiri::HTML(open(url_product))
+      html_product.xpath("//tr/td[@class='pdescr']").map do |product_node|
+        product = create_product(product_node)
+        category.add_product(product)
       end
+      #checking if there is a next product page in the same category
       html_product.xpath("//a").map do |is_next_node|
-        if is_next_node.text.delete("\n").delete(" ") == "Следующие15позиций"
-          url_prod = url_prod_const + is_next_node.xpath("./@href").to_s
+        if is_next_node.text.delete(" " "\n" "0-9").include? "Следующиепозиций"
+          url_product = $url_const + "/" + is_next_node.xpath("./@href").text
           break
-        else url_prod = false
+        else 
+          url_product = false
         end
       end
 
-    end     
+    end
 
   end
-end 
+end
